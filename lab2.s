286c9c77d693c20cbe84a294c6c50cab07aaaa9f
AGawas <arnavgawas707@gmail.com>
2026-02-05 14:58:57 -0500

    Clean up comments and reorganize lab2.s
    Refactor comments and code structure in lab2.s

--- lab2.s
+++ lab2.s
@@ -1,77 +1,52 @@
+            TTL Program Title for Listing Header Goes Here
 ;****************************************************************
-; TTL Program Title for Listing Header Goes Here
-;****************************************************************
-; Simple Arithmatics Problem in Assembly
-; Name:  Arnav Gawas
-; Date:  01-20-2026
-; Class:  CMPE-250
-; Section:  01, 2:00PM
+;Descriptive comment header goes here.
+;(What does the program do?)
+;Name:  Arnav Gawas
+;Date:  02/05/2026
+;Class:  CMPE-250
+;Section:  Lab Section X
 ;---------------------------------------------------------------
-; Keil Simulator Template for KL05
-; R. W. Melton
-; August 21, 2025
+;Keil Simulator Template for KL05
+;R. W. Melton
+;August 21, 2025
 ;****************************************************************
-
-; Assembler directives
+;Assembler directives
             THUMB
-            OPT    64      ; Turn on listing macro expansions
-
+            OPT    64  ;Turn on listing macro expansions
 ;****************************************************************
-; EQUates
-; Standard data masks
+;EQUates
+;Standard data masks
 BYTE_MASK         EQU  0xFF
 NIBBLE_MASK       EQU  0x0F
-; Standard data sizes (in bits)
+;Standard data sizes (in bits)
 BYTE_BITS         EQU  8
 NIBBLE_BITS       EQU  4
-; Architecture data sizes (in bytes)
-WORD_SIZE         EQU  4
-HALFWORD_SIZE     EQU  2
-; Architecture data masks
+;Architecture data sizes (in bytes)
+WORD_SIZE         EQU  4  ;Cortex-M0+
+HALFWORD_SIZE     EQU  2  ;Cortex-M0+
+;Architecture data masks
 HALFWORD_MASK     EQU  0xFFFF
-; Return
-RET_ADDR_T_MASK   EQU  1
-
+;Return                 
+RET_ADDR_T_MASK   EQU  1  ;Bit 0 of ret. addr. must be
+                          ;set for BX, BLX, or POP
+                          ;mask in thumb mode
 ;---------------------------------------------------------------
-; Vectors
-VECTOR_TABLE_SIZE EQU 0x000000C0
-VECTOR_SIZE       EQU 4
-
+;Vectors
+VECTOR_TABLE_SIZE EQU 0x000000C0  ;KL05
+VECTOR_SIZE       EQU 4           ;Bytes per vector
 ;---------------------------------------------------------------
-; CPU CONTROL: Control register
+;CPU CONTROL:  Control register
 CONTROL_SPSEL_MASK   EQU  2
 CONTROL_SPSEL_SHIFT  EQU  1
 CONTROL_nPRIV_MASK   EQU  1
 CONTROL_nPRIV_SHIFT  EQU  0
-
 ;---------------------------------------------------------------
-; CPU PRIMASK
+;CPU PRIMASK:  Interrupt mask register
 PRIMASK_PM_MASK   EQU  1
 PRIMASK_PM_SHIFT  EQU  0
-
 ;---------------------------------------------------------------
-; CPU PSR Masks
-APSR_MASK     EQU  0xF0000000
-APSR_SHIFT    EQU  28
-APSR_N_MASK   EQU  0x80000000
-APSR_N_SHIFT  EQU  31
-APSR_Z_MASK   EQU  0x40000000
-APSR_Z_SHIFT  EQU  30
-APSR_C_MASK   EQU  0x20000000
-APSR_C_SHIFT  EQU  29
-APSR_V_MASK   EQU  0x10000000
-APSR_V_SHIFT  EQU  28
-
-EPSR_MASK     EQU  0x01000000
-EPSR_SHIFT    EQU  24
-EPSR_T_MASK   EQU  0x01000000
-EPSR_T_SHIFT  EQU  24
-
-IPSR_MASK             EQU  0x0000003F
-IPSR_SHIFT            EQU  0
-IPSR_EXCEPTION_MASK   EQU  0x0000003F
-IPSR_EXCEPTION_SHIFT  EQU  0
-
+;CPU PSR:  Program status register
 PSR_N_MASK           EQU  APSR_N_MASK
 PSR_N_SHIFT          EQU  APSR_N_SHIFT
 PSR_Z_MASK           EQU  APSR_Z_MASK
@@ -84,31 +59,154 @@ PSR_T_MASK           EQU  EPSR_T_MASK
 PSR_T_SHIFT          EQU  EPSR_T_SHIFT
 PSR_EXCEPTION_MASK   EQU  IPSR_EXCEPTION_MASK
 PSR_EXCEPTION_SHIFT  EQU  IPSR_EXCEPTION_SHIFT
-
-;---------------------------------------------------------------
-; Stack
+;----------------------------------------------------------
+;Stack
 SSTACK_SIZE EQU  0x00000100
-
 ;****************************************************************
-; Program
-; Linker requires Reset_Handler
+;Program
+;Linker requires Reset_Handler
             AREA    MyCode,CODE,READONLY
             ENTRY
             EXPORT  Reset_Handler
-
-Reset_Handler  PROC
-            ; Initialize registers
+Reset_Handler  PROC {}
+main
+;---------------------------------------------------------------
+;Initialize registers
             BL      RegInit
-            ; Call expression calculator
-            BL      __main
-            ; Stay here indefinitely
+;>>>>> begin main program code <<<<<
+
+;------------------------------
+; EQUs for multiplication shifts
+;------------------------------
+MUL2    EQU 1   ; multiply by 2 -> shift left 1
+MUL3    EQU 1   ; multiply by 3 -> (x<<1)+x
+MUL4    EQU 2   ; multiply by 4 -> shift left 2
+
+;------------------------------
+; Load P and Q
+;------------------------------
+            LDR     R1,=P
+            LDR     R1,[R1]        ; R1 = P
+            LDR     R2,=Q
+            LDR     R2,[R2]        ; R2 = Q
+
+;------------------------------
+; Compute F = 3P + 2Q - 75
+; Byte overflow [-128,127]
+;------------------------------
+
+            ; 3P = (P << 1) + P
+            MOV     R3,R1
+            LSL     R3,R3,#MUL3
+            ADD     R3,R3,R1
+            CMP     R3,#127
+            BHI     F_OVF
+            CMP     R3,#-128
+            BLO     F_OVF
+
+            ; 2Q = Q << 1
+            MOV     R4,R2
+            LSL     R4,R4,#MUL2
+            CMP     R4,#127
+            BHI     F_OVF
+            CMP     R4,#-128
+            BLO     F_OVF
+
+            ; 3P + 2Q
+            ADD     R3,R3,R4
+            CMP     R3,#127
+            BHI     F_OVF
+            CMP     R3,#-128
+            BLO     F_OVF
+
+            ; -75
+            LDR     R5,=const_F
+            LDR     R5,[R5]
+            SUB     R3,R3,R5
+            CMP     R3,#127
+            BHI     F_OVF
+            CMP     R3,#-128
+            BLO     F_OVF
+
+            ; Store F
+            LDR     R6,=F
+            STR     R3,[R6]
+            B       F_DONE
+
+F_OVF:
+            LDR     R6,=F
+            MOV     R3,#0
+            STR     R3,[R6]
+
+F_DONE:
+
+;------------------------------
+; Compute G = 2P - 4Q + 63
+; Use MSB and V flag
+;------------------------------
+            ; 2P
+            MOV     R3,R1
+            LSL     R3,R3,#MUL2
+            AND     R3,R3,#0xFF00
+
+            ; 4Q
+            MOV     R4,R2
+            LSL     R4,R4,#MUL4
+            AND     R4,R4,#0xFF00
+
+            ; 2P - 4Q
+            SUBS    R3,R3,R4
+            BVS     G_OVF
+
+            ; +63
+            LDR     R5,=const_G
+            LDR     R5,[R5]
+            ADDS    R3,R3,R5
+            BVS     G_OVF
+
+            ; Store G
+            LDR     R6,=G
+            STR     R3,[R6]
+            B       G_DONE
+
+G_OVF:
+            LDR     R6,=G
+            MOV     R3,#0
+            STR     R3,[R6]
+
+G_DONE:
+
+;------------------------------
+; Compute Result = F + G
+;------------------------------
+            LDR     R3,=F
+            LDR     R3,[R3]
+            LDR     R4,=G
+            LDR     R4,[R4]
+            ADD     R3,R3,R4
+            CMP     R3,#127
+            BHI     RESULT_OVF
+            CMP     R3,#-128
+            BLO     RESULT_OVF
+
+            LDR     R5,=Result
+            STR     R3,[R5]
+            B       END_MAIN
+
+RESULT_OVF:
+            LDR     R5,=Result
+            MOV     R3,#0
+            STR     R3,[R5]
+
+END_MAIN:
+;>>>>>   end main program code <<<<<
             B       .
-            ENDP
+            ENDP    ;main
 
 ;---------------------------------------------------------------
-RegInit     PROC
+RegInit     PROC  {}
 ;********************************************************************
-; Initializes registers
+;Initializes registers (template code kept intact)
 ;********************************************************************
             PUSH    {LR}
             LDR     R1,=0x11111111
@@ -135,67 +233,24 @@ RegInit     PROC
             MSR     APSR,R0
             LDR     R0,=0x05250821
             POP     {PC}
-            ENDP
+            ENDP    ;RegInit
 
 ;****************************************************************
-; Expression Calculator
-;****************************************************************
-            AREA    ExpressionCalc, CODE, READONLY
-            EXPORT  __main
-
-DIV4        EQU     2
-MULT2       EQU     1
-
-__main
-        ; Initialize operands
-        MOVS     R0, #45
-        MOVS    R1, #6
-        MOVS     R2, #13
-        MOVS     R3, #7
-        MOVS     R4, #65
-        MOVS     R5, #33
-
-        ; Compute -R2 / DIV4
-        RSBS    R6, R2, #0      ; R6 = -R2
-        ASRS     R6, R6, #DIV4   ; R6 = R6 >> DIV4 (arithmetic shift)
-
-        ; Compute R3 * MULT2 + R3
-        LSLS     R7, R3, #MULT2  ; R7 = R3 << MULT2
-        ADDS     R7, R7, R3      ; R7 = R7 + R3
-
-        ; Compute final expression: R0 + R1 + R6 - R7 - R4 + R5
-        ADDS     R0, R0, R1
-        ADDS     R0, R0, R6
-        SUBS     R0, R0, R7
-        SUBS     R0, R0, R4
-        ADDS     R0, R0, R5
-
-        ; Infinite loop to hold result
-        B       .
+;Constants
+            AREA    MyConst,DATA,READONLY
+;>>>>> begin constants here <<<<<
+const_F     DCD 75
+const_G     DCD 63
+;>>>>>   end constants here <<<<<
 
 ;****************************************************************
-; Vector Table
-;****************************************************************
-            AREA    RESET, DATA, READONLY
-            EXPORT  __Vectors
-            EXPORT  __Vectors_End
-            EXPORT  __Vectors_Size
-
-__Vectors 
-            DCD    __initial_sp       ; 00:end of stack
-            DCD    Reset_Handler      ; reset vector
-            SPACE  (VECTOR_TABLE_SIZE - (2 * VECTOR_SIZE))
-
-__Vectors_End
-__Vectors_Size  EQU     __Vectors_End - __Vectors
-            ALIGN
-
-;****************************************************************
-; System Stack
-;****************************************************************
-            AREA    |.ARM.__at_0x1FFFFC00|,DATA,READWRITE,ALIGN=3
-            EXPORT  __initial_sp
-
-Stack_Mem   SPACE   SSTACK_SIZE
-__initial_sp
+;Variables
+            AREA    MyData,DATA,READWRITE
+;>>>>> begin variables here <<<<<
+P       DCD 5
+Q       DCD 10
+F       DCD 0
+G       DCD 0
+Result  DCD 0
+;>>>>>   end variables here <<<<<
             END
